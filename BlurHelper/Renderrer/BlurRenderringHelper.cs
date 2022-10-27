c5aeee260cccb8fcbb81307de6ab31cb8cd5ea96
RandomiaGaming <RandomiaGaming@gmail.com>
2022-10-27 13:37:18 -0700

    Changes

--- BlurHelper/Renderrer/BlurRenderringHelper.cs
+++ BlurHelper/Renderrer/BlurRenderringHelper.cs
@@ -2,10 +2,9 @@
 {
     public static class BlurRenderringHelper
     {
-        public static void Render(string videoFrameDirectoryPath, string saveDataPath)
+        public static void Render(string inputFrameDirectoryPath, string outputFrameDirectoryPath, uint frameCount, System.Drawing.Size lowresSize, bool deleteSource, string saveDataPath)
         {
-            System.Diagnostics.Stopwatch stopwatch = new System.Diagnostics.Stopwatch();
-            stopwatch.Start();
+            System.Console.WriteLine("Beginning opperation...");
 
             CPUHelper.SetAffinityHighest();
 
@@ -13,19 +12,15 @@
 
             SaveData saveData = SaveData.Deserialize(System.IO.File.ReadAllText(saveDataPath));
 
-            System.Drawing.Size lowresSize = new System.Drawing.Size(192, 108);
+            RenderFrameInputPacket[] parameters = new RenderFrameInputPacket[frameCount];
 
-            RenderFrameInputPacket[] parameters = new RenderFrameInputPacket[252433];
-
-            System.Console.WriteLine("Beginning opperation...");
-
-            for (int i = 0; i < 252433; i++)
+            for (uint i = 0; i < frameCount; i++)
             {
-                parameters[i].sourceFramePath = $"D:\\Frames\\{i}.png";
-                parameters[i].destinationFramePath = $"D:\\Renders\\{i}.png";
-                parameters[i].frameData = saveData.GetFrameData((uint)i);
+                parameters[i].sourceFramePath = $"{inputFrameDirectoryPath}\\{i + 1}.png";
+                parameters[i].destinationFramePath = $"{outputFrameDirectoryPath}\\{i + 1}.png";
+                parameters[i].frameData = saveData.GetFrameData(i);
                 parameters[i].lowresSize = lowresSize;
-                parameters[i].deleteSource = true;
+                parameters[i].deleteSource = deleteSource;
             }
 
             System.Console.WriteLine("Beginning render...");
@@ -43,11 +38,6 @@
                 System.Console.WriteLine($"Failed to render frame \"{parameter.sourceFramePath}\" due to exception \"{exception.Message}\". \"{completedParameterCount}\" of \"{totalParameterCount}\" complete.");
                 System.IO.File.WriteAllText(parameter.destinationFramePath + ".txt", $"Failed to render frame \"{parameter.sourceFramePath}\" due to exception \"{exception.Message}\". \"{completedParameterCount}\" of \"{totalParameterCount}\" complete.");
             }, 1, 100);
-
-            stopwatch.Stop();
-
-            System.Console.WriteLine(stopwatch.ElapsedTicks);
-            System.Console.ReadLine();
         }
         private struct RenderFrameInputPacket
         {
@@ -61,13 +51,14 @@
         {
             if (frameData is null)
             {
-                System.IO.File.Copy(sourceFramePath, destinationFramePath);
-
                 if (deleteSource)
                 {
-                    System.IO.File.Delete(sourceFramePath);
+                    System.IO.File.Move(sourceFramePath, destinationFramePath);
                 }
-
+                else
+                {
+                    System.IO.File.Copy(sourceFramePath, destinationFramePath);
+                }
                 return;
             }
 
--- Program.cs
+++ Program.cs
@@ -1,11 +1,35 @@
 ﻿using BlurHelper;
+using System.Linq;
 public static class Program
 {
     [System.STAThread]
     public static void Main()
     {
-        /*BlurHelperGame blurHelperGame = new BlurHelperGame("E:\\Frames", "E:\\TITOAAOGMI99MOL.blurhelper");
-        blurHelperGame.Run();*/
-        BlurRenderringHelper.Render("D:\\Frames", "D:\\TITOAAOGMI99MOL Final.blurhelper");
+        BlurRenderringHelper.Render("E:\\TITOAAOGMI99MOL\\Unblurred Frames", "E:\\TITOAAOGMI99MOL\\Frames", (uint)System.IO.Directory.GetFiles("E:\\TITOAAOGMI99MOL\\Unblurred Frames").LongLength, new System.Drawing.Size(192, 108), true, "E:\\TITOAAOGMI99MOL\\Keyframe Data.txt");
     }
-} 
+    public static string ConcatStringArray(string[] array)
+    {
+        long outputLength = 0;
+        foreach (string str in array)
+        {
+            outputLength += str.Length;
+        }
+        char[] outputCharArray = new char[outputLength];
+        long currentIndex = 0;
+        foreach (string str in array)
+        {
+            System.Array.Copy(str.ToCharArray(), 0, outputCharArray, currentIndex, str.Length);
+            currentIndex += str.Length;
+        }
+        return new string(outputCharArray);
+    }
+    public static int[] ParseStringArray(string[] array)
+    {
+        int[] output = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            output[i] = int.Parse(array[i]);
+        }
+        return output;
+    }
+}
